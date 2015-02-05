d715281b8552e84a6ad12db40a0b89d6c1bf3632
Benjamin Dehli <superelg@gmail.com>
2015-02-05 23:57:49 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,179 @@
+<html>
+	<head>
+		<link href="css/standard.css" rel="stylesheet">
+	</head>
+	<body>
+		<header>
+			<nav class="border fg-black">
+				<ul>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+			</nav>
+			<nav class="bg-blue border fg-white">
+				<ul>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+			</nav>
+			<nav class="bg-green border fg-white">
+				<ul>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+			</nav>
+			<nav class="bg-red border fg-white">
+				<ul>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+			</nav>
+			<nav class="bg-yellow border fg-white">
+				<ul>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+			</nav>
+			<nav class="bg-black border fg-white">
+				<ul>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+			</nav>
+		</header>
+
+		<aside class="left bg-yellow border col-3">
+		håååkon
+		</aside>
+		<main class="col-9">
+		
+		<section class="bg-blue border">
+			<div class="container">
+				<div class="col-5">
+					<h1>col-4</h1>
+				</div>
+				<div class="col-2">
+					<div class="panel">
+						<h2>col-2</h2>
+						<a class="button round fg-white bg-red">Knapp</a>
+						<a class="button round fg-white bg-blue">Knapp</a>
+						<a class="button round fg-white bg-green">Knapp</a>
+					</div>
+				</div>
+				<div class="col-3">
+					<h3>col-5</h3>
+					<ul>
+						<li>fd</li>
+						<li>sdf</li>
+						<li>dsf</li>
+						<li>sdf</li>
+					</ul>
+				</div>
+				<div class="col-2">
+					<p>col-1</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</section>
+		<section>
+			<div class="col-5">
+				<h1>col-4</h1>
+				<form role="form">
+				<label for="radio1">radio1</label>
+				<input type="radio" name="radio" id="radio1" value="1">
+				<label for="radio2">radio2</label>
+				<input type="radio" name="radio" id="radio2" value)"2">
+				<label for="check1">checkbox1</label>
+				<input type="checkbox" id="check1">
+				</form>
+				
+			</div>
+			<div class="col-2">
+				<div class="panel round">
+					<h2>col-2</h2>
+					<a class="button round fg-white bg-red">Knapp</a>
+					<a class="button round fg-white bg-blue">Knapp</a>
+					<a class="button round fg-white bg-green">Knapp</a>
+				</div>
+			</div>
+			<div class="col-3">
+			<div class="panel round">
+				<h3>col-5</h3>
+				<ul>
+					<li>fd</li>
+					<li>sdf</li>
+					<li>dsf</li>
+					<li>sdf</li>
+				</ul>
+			</div>
+			</div>
+			<div class="col-2">
+				<h2>hoy</h2>
+				<p>hoyoy</p>
+				hoyhoy
+			</div>
+			<div class="clearfix"></div>
+			
+		</section>
+		<section>
+			<div class="col-5">
+				<h1>col-4</h1>
+			</div>
+			<div class="col-2">
+				<h2>col-2</h2>
+				<a class="button round fg-white bg-red">Knapp</a>
+				<a class="button round fg-white bg-blue">Knapp</a>
+				<a class="button round fg-white bg-green">Knapp</a>
+			</div>
+			<div class="col-3">
+				<h3>col-5</h3>
+				<ul>
+					<li>fd</li>
+					<li>sdf</li>
+					<li>dsf</li>
+					<li>sdf</li>
+				</ul>
+			</div>
+			<div class="col-2">
+				<h2>hoy</h2>
+				<p>hoyoy</p>
+				hoyhoy
+			</div>
+			<div class="clearfix"></div>
+		</section>
+		<section class="bg-red border">
+			<div class="container">
+				
+				<div class="col-4">
+					<h1>H1 tekst</h1>
+				</div>
+				<div class="col-2">
+					<h2>H2 tekst</h2>
+				</div>
+				<div class="col-5">
+					<h3>H3 tekst</h3>
+				</div>
+				<div class="col-1">
+					<p>P element</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</section>
+	</div>
+	</main>
+	<footer class="bg-black fg-white text-center border fullscreen col-12">
+		<div class="container">
+			<div class="col-4">hoy</div>
+			<div class="col-4">dsad</div>
+			<div class="col-4">hahahadhisof</div>
+			<div class="col-12">Benjamin Dehli</div>
+		</div>
+	</footer>
+</body>
+</html>
